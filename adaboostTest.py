50c5dfc38f707a720423971af10521f1d9b524bb
Sahaj Bhakta <85909754+BajuMcBites@users.noreply.github.com>
2023-11-16 12:46:20 -0600

    changes to test

--- adaboostTest.py
+++ adaboostTest.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     [test_data, test_labels] = t.combineBatches([test_batch])
     test_images = t.convertDataToImages(test_data, [32, 32])
 
-    model_names = ["model_0.pkl", "model_1.pkl", "model_2.pkl", "model_3.pkl", "model_4.pkl", "model_5.pkl", "model_6.pkl", "model_7.pkl", "model_8.pkl", "model_9.pkl"]
+    model_names = ["models/model_0.pkl", "models/model_1.pkl", "models/model_2.pkl", "models/model_3.pkl", "models/model_4.pkl", "models/model_5.pkl", "models/model_6.pkl", "models/model_7.pkl", "models/model_8.pkl", "models/model_9.pkl"]
     models = [ada.ImAdaBoost(0, 0) for i in range(len(model_names))]
     for i in range(10):
         models[i].reconstruct_model(model_names[i])
